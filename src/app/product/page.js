57f35dd3db3b8b19fc740d809922907d9f8c3192
jorgeochipinti97 <jorgeochipinti97@gmail.com>
2024-06-18 11:58:26 -0300

    @'

--- src/app/product/page.js
+++ src/app/product/page.js
@@ -11,19 +11,23 @@ import {
 } from "@/components/ui/carousel";
 import { formatPrice } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-const page = () => {
+import useCartStore from "@/hook/useCartStore";
+const Page = () => {
   const [producto, setProducto] = useState({});
   const searchParams = useSearchParams();
   const { products } = useProducts();
+  const agregarProducto = useCartStore((state) => state.agregarProducto);
 
   useEffect(() => {
     const d = searchParams.get("_id");
     d && products && setProducto(products.filter((e) => e._id == d)[0]);
   }, [products]);
-  const { push} = useRouter()
+  const { push } = useRouter();
   return (
     <div className=" py-10">
-      <Button className='m-5'onClick={()=>push('/')} variant='outline'>Volver al inicio</Button>
+      <Button className="m-5" onClick={() => push("/")} variant="outline">
+        Volver al inicio
+      </Button>
       {producto && (
         <div className="grid md:grid-cols-2 grid-cols-1">
           <div className="w-full flex justify-center">
@@ -31,7 +35,7 @@ const page = () => {
               <CarouselContent>
                 {producto.images &&
                   producto.images.map((e) => (
-                    <CarouselItem>
+                    <CarouselItem key={e}>
                       <img src={e} className="rounded-xl" />
                     </CarouselItem>
                   ))}
@@ -41,13 +45,28 @@ const page = () => {
             </Carousel>
           </div>
           <div className="md:mx-0 mx-2">
-            <p className="text-4xl md:text-7xl text-center  tracking-tighter md:mt-0 mt-10 font-bold">{producto.title}</p>
-            <p className="text-md  text-center tracking-tighter mt-5 ">{producto.description}</p>
-            <p className="text-2xl md:text-4xl text-center  tracking-tighter mt-5">{formatPrice(producto.price)}</p>
+            <p className="text-4xl md:text-7xl text-center  tracking-tighter md:mt-0 mt-10 font-bold">
+              {producto.title}
+            </p>
+            <p className="text-md  text-center tracking-tighter mt-5 ">
+              {producto.description}
+            </p>
+            <p className="text-2xl md:text-4xl text-center  tracking-tighter mt-5">
+              {formatPrice(producto.price)}
+            </p>
             <div className="mt-5 flex justify-center md:justify-start">
-
-            <Button>Agregar al carrito</Button>
-          </div>
+              <Button
+                onClick={() => {
+                  agregarProducto({
+                    title: producto.title,
+                    price: producto.price,
+                    images: producto.images,
+                  });
+                }}
+              >
+                Agregar al carrito
+              </Button>
+            </div>
           </div>
         </div>
       )}
@@ -55,4 +74,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
